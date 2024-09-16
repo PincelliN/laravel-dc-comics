651b1748714ec1de84855b9ed7539fc49fe0beff
Nicolò Pincelli <pincelli.n@gmail.com>
2024-09-16 19:22:41 +0200

    Start install composer and npm ,generate key ,generate db, make migration, make model and seeder

---
+++ database/migrations/2024_09_16_163254_create_comics_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Egulias\EmailValidator\Result\Reason\CharNotAllowed;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',100);
+            $table->string('slug',100)->uniqid();
+            $table->text('description')->nullable();
+            $table->string('thumb',200);
+            $table->string('price',10);
+            $table->string('series',100);
+            $table->date('sale_date',10);
+            $table->string('type',50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comics');
+    }
+};
---
+++ database/seeders/ComicSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Comic;
+use App\Functions\Helper;
+use Illuminate\Support\Str;
+
+class ComicSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $comics=config('comics');
+        foreach($comics as $comic){
+            $new_comic=new Comic();
+            $new_comic->title=$comic['title'];
+            $new_comic->slug=Helper::generateSlug($new_comic->title,Comic::class);
+            $new_comic->description=$comic['description'];
+            $new_comic->thumb=$comic['thumb'];
+            $new_comic->price=$comic['price'];
+            $new_comic->series=$comic['series'];
+            $new_comic->sale_date=$comic['sale_date'];
+            $new_comic->type=$comic['type'];
+            /* dump($new_comic); */
+            $new_comic->save();
+        }
+    }
+}
